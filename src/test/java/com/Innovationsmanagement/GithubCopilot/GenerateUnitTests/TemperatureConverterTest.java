4693d9553a6601528977e6bc4c13ecdffebbd334
Lukas Strauß <lukas.strauss@hdi.de>
2025-08-19 18:22:11 +0200

    Presentation Copilot functions

---
+++ src/test/java/com/Innovationsmanagement/GithubCopilot/GenerateUnitTests/TemperatureConverterTest.java
@@ -0,0 +1,9 @@
+package com.Innovationsmanagement.GithubCopilot.GenerateUnitTests;
+
+/*
+    Zeile 8 umbrechen und schauen, was der Copilot vorschlägt.
+ */
+
+
+public class TemperatureConverterTest {
+}
